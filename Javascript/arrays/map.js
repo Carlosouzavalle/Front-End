1b0db9789661159efdf9ec4e319cb744d75e4a63
carlosouzavalle <carlosouzavalle@gmail.com>
2025-11-11 21:53:10 -0300

    um pouco mais sobre mais de map

--- Javascript/arrays/map.js
+++ Javascript/arrays/map.js
@@ -19,3 +19,41 @@ console.log(dobrados)
 // map() não altera o array original, ele cria um novo.
 //======================================================================================
 
+const usuarios = [
+  {nome: 'carlos', idade: 27 },
+  {nome: 'ana', idade: 27 },
+  {nome: 'bia', idade: 27 },
+]
+
+const nomes = usuarios.map(usuario => usuario.nome)
+
+console.log(nomes)
+
+//===============================================================================================
+
+// exemplo com indice
+
+const numeros2 = [10,20,30]
+const resultado2 = numeros2.map((valor, indice) => valor + indice)
+
+console.log(resultado2)
+
+
+//==============================================================================================================
+const numeros3 = [1, 2, 3]
+const errado = numeros3.map(num => { num * 2 })
+const certo = numeros.map(num => num * 2)
+console.log(errado) // [undefined, undefined, undefined]
+console.log(certo)
+
+
+// 💡 Dica prática
+
+// Use map() quando:
+// Você quer transformar dados (ex: converter, formatar, multiplicar etc.).
+// Você precisa gerar um novo array a partir de outro.
+
+
+// ⚠️ Importante:
+// map() sempre retorna um novo array.
+// Se você não retornar nada na função, o novo array vai ter valores undefined.
