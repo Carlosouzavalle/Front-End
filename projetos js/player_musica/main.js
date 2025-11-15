b43e013a468a9f3127d5d50e865cd1445977b6b2
carlosouzavalle <carlosouzavalle@gmail.com>
2025-11-15 17:10:26 -0300

    feat: player de musica add mais algumas coisas novas

--- projetos js/player_musica/main.js
+++ projetos js/player_musica/main.js
@@ -1,8 +1,17 @@
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px"
-    console.log('teste')
+    document.getElementById("container-seach-upgrade").style.marginLeft = "200px"
+    if (window.innerWidth < 768) {
+        document.getElementById("seach-bar").style.width = "200px"
+        document.getElementById("seach-bar").style.height = "30px"
+        document.getElementById("upgrade-container").style.display = "none"
+    }
 }
 
 function closeNav() {
     document.getElementById("mySidebar").style.width = "0"
+    if (window.innerWidth < 768) {
+        document.getElementById("container-seach-upgrade").style.marginLeft = "0";
+        document.getElementById("upgrade-container").style.display = "block";
+    }
 }
