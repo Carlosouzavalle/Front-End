41bc4cf718b3068b2ba84111f0ece11476ec4a3d
carlosouzavalle <carlosouzavalle@gmail.com>
2026-01-13 19:14:18 -0300

    revisando conteudos

--- Javascript/callback/main2.js
+++ Javascript/callback/main2.js
@@ -1,19 +1,29 @@
+// uma função callback é uma função passada para ser executada em algum momento mais tarde
+// um dos problemas da callback é que você coloca uma função dentro da outra e isso fca muito confuso
 
 
-const login = (email, password, onSuces,setError) => {
+const login = (email, password, onSucess) => {
     setTimeout(() => {
-        const error = false
-
-        if (error) {
-            return setError(new Error('Senha errada/invalida '))
-        }
-
-
-        console.log('Usuario logado')
-        onSuces({ email, password })
+        console.log("usuario logado!")
+        onSucess({email, password})
     }, 1000)
 }
 
-const user = login('carlos@legal', 123123, (user) => {
-    console.log({user})
+const getDetails = (email, onSuccess) => {
+    setTimeout(() => {
+        //console.log({nome: nome, idade: idade, tell: tell})
+        onSuccess({nome: 'carlos', idade: 27, tell: 123123123, email}) // callback
+    }, 2000)
+}
+
+const user = login("carlos@kakka", 123123, (user) => {
+    getDetails(user.email, (details) => {
+        console.log({details})
+    })
 })
+
+
+
+// Posição	Parâmetro	O que ele deve ser
+// 1º	email	o dado (ex: string do email)
+// 2º	onSuccess	a função callback
