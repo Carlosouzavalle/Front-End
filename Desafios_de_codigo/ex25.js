0c3b360eaf361053f61b7230484fa067f708ba20
carlosouzavalle <carlosouzavalle@gmail.com>
2026-01-16 10:14:43 -0300

    resolvendo desafios

---
+++ Desafios_de_codigo/ex25.js
@@ -0,0 +1,13 @@
+// let valuee = "carlos vale "
+// let get_value = valuee.toLowerCase().replace(/\s+/g, "")
+// let result = get_value.includes("eduardo")
+// console.log(result)
+
+
+let value2 = "carlos edua rdo"
+let get_value2 = /eduardo/.test(value2.toLowerCase().replace(/\s+/g, ""))
+console.log(get_value2)
+console.log(value2)
+
+// includes
+// regex 
