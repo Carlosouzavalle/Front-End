dd4b7e0ea3cb5feb815dab85802ede6872680ef9
carlosouzavalle <carlosouzavalle@gmail.com>
2026-03-01 12:36:57 -0300

    feat: mudanças no header, cards, e sponrs

--- react_projetos/loja_musica/loja_musica/app/components/header.components.jsx
+++ react_projetos/loja_musica/loja_musica/app/components/header.components.jsx
@@ -2,7 +2,7 @@ import { FaSearch } from "react-icons/fa";
 
 const Header = () => {
     return (
-        <header className="bg-red-500">
+        <header className="bg-[#0F0F0F]">
             <div className="flex justify-between items-center p-8">
 
                 <div className="logo">
@@ -11,7 +11,7 @@ const Header = () => {
                 </div>
 
                 <div className="search_container">
-                    <div className="search_box">
+                    <div className="search_box ">
                         <input type="search" placeholder="Buscar instrumentos..." />
                     </div>
                     <div className="btn_div">
@@ -22,13 +22,13 @@ const Header = () => {
                 </div>
 
                 <nav className="nav_container">
-                    <ul className="nav_links">
+                    <ul className="nav-links flex gap-3 justify-center items-center">
                         <li><a href="#">Inicio</a></li>
                         <li><a href="#">Instrumentos</a></li>
                         <li><a href="#">Carrinho</a></li>
                         <li><a href="#">Promoções</a></li>
                         <li><a href="#">Contato</a></li>
-                        <li className="buy_now"><a href="#">Comprar Agora</a></li>
+                        <li className=" p-2 rounded-sm bg-[#E10600] hover:bg-[#FF3C38]"><a href="#">Comprar Agora</a></li>
                     </ul>
                 </nav>
 
